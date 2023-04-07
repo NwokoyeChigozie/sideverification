5b52dc0842dfae630989df258c3747ad43d90c9b
Nwokoye Chigozie <gregoflash05@gmail.com>
2023-04-07 16:13:39 +0100

    update

--- cronjobs/base.go
+++ cronjobs/base.go
@@ -3,6 +3,7 @@ package cronjobs
 import (
 	"fmt"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/vesicash/verification-ms/external/request"
@@ -23,22 +24,24 @@ type CronJobObject struct {
 	Interval time.Duration
 }
 
-func Scheduler(extReq request.ExternalRequest, db postgresql.Databases, cronJob CronJob, interval time.Duration) {
+func Scheduler(extReq request.ExternalRequest, db postgresql.Databases, mutex *sync.Mutex, cronJob CronJob, interval time.Duration) {
 	for {
+		mutex.Lock()
 		cronJob(extReq, db)
+		mutex.Unlock()
 		time.Sleep(interval)
 	}
 }
 
 func SetupCronJobs(extReq request.ExternalRequest, db postgresql.Databases, selectedJobs []string) {
-
+	mutex := &sync.Mutex{}
 	for _, v := range selectedJobs {
 		jobName := strings.ToLower(v)
 		cronJob, ok := cronJobs[jobName]
 
 		if ok {
 			utility.LogAndPrint(extReq.Logger, fmt.Sprintf("starting cronjob: %s", jobName))
-			go Scheduler(extReq, db, cronJob.CronJob, cronJob.Interval)
+			go Scheduler(extReq, db, mutex, cronJob.CronJob, cronJob.Interval)
 		} else {
 			utility.LogAndPrint(extReq.Logger, fmt.Sprintf("Cronjob not found: %s", jobName))
 		}
