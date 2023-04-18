9607b94a3f30e1bd7bbe7eaecd46f0a945172891
Nwokoye Chigozie <gregoflash05@gmail.com>
2023-04-18 10:18:21 +0100

    fix

--- services/verification/bvn_verification.go
+++ services/verification/bvn_verification.go
@@ -55,10 +55,6 @@ func VerifyBVNService(extReq request.ExternalRequest, Logger *utility.Logger, db
 
 	userCredentials, code, err := GetUserCredential(0, int(user.AccountID), "bvn", extReq)
 	if err != nil {
-		if code == http.StatusInternalServerError {
-			return false, code, err
-		}
-
 		_, code, err := CreateUserCredential(int(user.AccountID), "bvn", "", bvn, extReq)
 		if err != nil {
 			return false, code, err
